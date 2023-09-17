29beb49e8ba807b3dfbe6043078064178beb59da
romanwarrier982 <romanlill9227@gmail.com>
2023-09-17 13:45:22 +0100

    Permission API WIP

--- backend/app/Http/Controllers/RoleController.php
+++ backend/app/Http/Controllers/RoleController.php
@@ -2,9 +2,123 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use Dotenv\Validator;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('JWT');
+    }
+    /**
+     * Fetch products
+     * @param NA
+     * @return JSON response
+     */
+    public function index()
+    {
+        $roles = Role::paginate(10);
+        return response()->json(["status" => "success", "count" => count($roles), "data" => $roles]);
+    }
+
+    /**
+     * Upload products
+     * @param $request
+     * @return JSON response
+     */
+    public function add(Request $request)
+    {
+        $response = [];
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(["status" => "failed", "message" => "Validation error", "errors" => $validator->errors()]);
+        }
+    
+
+        Role::create([
+            'name' => $request->name,
+            
+        ]);
+
+        return response()->json(["status" => "success", "message" => "Success! role(s) added"]);
+    }
+
+    /**
+     * update product
+     * @param NA
+     * @return JSON response
+     */
+    public function update(Request $request)
+    {
+
+
+       
+
+        Role::where('id', $request->id)->update([
+            'name' => $request->name,
+         
+        ]);
+
+        return response()->json(["status" => "success", "message" => "Success! role(s) updated"]);
+    }
+
+    /**
+     * Delete product
+     * @param NA
+     * @return JSON response
+     */
+    public function delete(Request $request)
+    {
+
+        Role::where('id', $request->id)->delete();
+        return response()->json(["status" => "Successfully Deleted Data",]);
+    }
+
+
+
+    /**
+     * Search product
+     * @param NA
+     * @return JSON response
+     */
+    public function search(Request $request)
+    {
+
+        return Product::where('name', 'LIKE', '%' . $request->get('searchKey') . '%')->paginate(10);
+    }
+
+    /**
+     * Create Path product
+     * @param NA
+     * @return String response
+     */
+    public function createImagePath($images)
+    {
+        $filename = time() . rand() . '.' . $images->getClientOriginalExtension();
+        $images->move('uploads/', $filename);
+
+        return $filename;
+    }
+
+
+    /**
+     * Fetch products
+     * @param NA
+     * @return JSON response
+     */
+    public function userList()
+    {
+        $user = User::with('role')->paginate(10);
+        return response()->json(["status" => "success", "data" => $user]);
+    }
 }
